b2cae986d1ef290db9c554da23af3b3ab8c49b1d
udayshankar1 <udaybsc@gmail.com>
2014-01-22 14:38:18 +0530

    new changes in dashboard page

---
+++ src/main/java/com/mangium/primecomponents/DashboardBean.java
@@ -0,0 +1,60 @@
+package com.mangium.primecomponents;
+import java.io.Serializable;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.DashboardReorderEvent;
+import org.primefaces.model.DashboardColumn;
+import org.primefaces.model.DashboardModel;
+import org.primefaces.model.DefaultDashboardColumn;
+import org.primefaces.model.DefaultDashboardModel;
+
+/**
+ *
+ * @author uday
+ */
+
+@ManagedBean
+@RequestScoped
+public class DashboardBean implements Serializable {  
+  
+    private DashboardModel model;  
+      
+    public DashboardBean() {  
+        model = new DefaultDashboardModel();  
+        DashboardColumn column1 = new DefaultDashboardColumn();  
+        DashboardColumn column2 = new DefaultDashboardColumn();  
+        DashboardColumn column3 = new DefaultDashboardColumn();  
+          
+        column1.addWidget("sports");  
+        column1.addWidget("finance");  
+          
+        column2.addWidget("lifestyle");  
+        column2.addWidget("weather");  
+          
+        column3.addWidget("politics");  
+  
+        model.addColumn(column1);  
+        model.addColumn(column2);  
+        model.addColumn(column3);  
+    }  
+      
+    public void handleReorder(DashboardReorderEvent event) {  
+        FacesMessage message = new FacesMessage();  
+        message.setSeverity(FacesMessage.SEVERITY_INFO);  
+        message.setSummary("Reordered: " + event.getWidgetId());  
+        message.setDetail("Item index: " + event.getItemIndex() + ", Column index: " + event.getColumnIndex() + ", Sender index: " + event.getSenderColumnIndex());  
+          
+        addMessage(message);  
+    }  
+      
+      
+    private void addMessage(FacesMessage message) {  
+        FacesContext.getCurrentInstance().addMessage(null, message);  
+    }  
+      
+    public DashboardModel getModel() {  
+        return model;  
+    }  
+}  
